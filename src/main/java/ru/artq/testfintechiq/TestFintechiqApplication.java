7f73d6ce7be961d3d3302dc99712e46f71bd97ba
Artem Kazakov <wiqiw90@gmail.com>
2025-04-14 12:04:55 +0800

    first commit

---
+++ src/main/java/ru/artq/testfintechiq/TestFintechiqApplication.java
@@ -0,0 +1,13 @@
+package ru.artq.testfintechiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFintechiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestFintechiqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/artq/testfintechiq/TestFintechiqApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artq.testfintechiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFintechiqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
